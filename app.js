a395de048d4b2649045e643e73222e35917f80b9
amarudaicz <amarudaicz10@gmail.com>
2024-06-15 21:15:04 -0300

    sss

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const WebSocket = require('websocket');
-const {createServer} = require('node:http')
+import WebSocket, { WebSocketServer } from 'ws';
+const PORT = process.env.PORT || 3000
 
-const server = createServer(function(request, response) {
-    response.writeHead(404);
-    response.end();
+const wss = new WebSocketServer({
+  port: PORT,
 });
 
-
-const wss = new WebSocket.server({httpServer:server, autoAcceptConnections:true});
 const clients = new Set();
  
 wss.on("connection", (ws) => {
@@ -32,7 +29,3 @@ wss.on("connection", (ws) => {
 
 console.log('SERVER READY', wss);
 
-const PORT = process.env.PORT || 3000
-server.listen(PORT, function() {
-    console.log((new Date()) + ' Server is listening on ', PORT);
-});
