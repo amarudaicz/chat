90e9fe3cc29ef45d1083bdbe926daad0db4b03b9
amarudaicz10 <amarudaicz10@gmail.com>
2024-06-19 01:46:41 +0000

    fhh

--- app.js
+++ app.js
@@ -1,29 +1,51 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const wss = new WebSocket.Server({
   port: PORT,
 });
 
 const clients = new Set();
- 
-wss.on("connection", (ws) => {
-  console.log("Nuevo cliente conectado");
-  clients.add(ws);
 
-  ws.on('message', message => {
-    const data = JSON.parse(message);
-    wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({ username: data.username, message: data.message }));
+function createWebSocketServer(){
+
+  wss.on("connection", (ws) => {
+    console.log("Nuevo cliente conectado");
+
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send("¡Se ha conectado un nuevo cliente!");
       }
+    });
+
+    ws.on("message", (message) => {
+      const data = JSON.parse(message);
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          if (data.image) {
+            client.send(
+              JSON.stringify({
+                username: data.username,
+                image: data.image,
+                message: data.message,
+              })
+            );
+          } else {
+            client.send(
+              JSON.stringify({ username: data.username, message: data.message })
+            );
+          }
+        }
+      });
+    });
+
+    ws.on("close", () => {
+      console.log("Cliente desconectado");
     });
   });
 
-  ws.on("close", () => {
-    clients.delete(ws);
-  });
-});
+  console.log(`Servidor ejecutando en el puerto ${PORT}`);
+}
 
-console.log('SERVER READY', wss);
+createWebSocketServer();
